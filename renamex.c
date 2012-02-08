3672afe0be30fae743904d83f20bc7ceecbc8262
itouhiro <itouhiro@gmail.com>
2012-02-08 23:24:30 +0900

    version 0.3
    able to delete extension
    add a Windows executable file

--- renamex.c
+++ renamex.c
@@ -1,15 +1,18 @@
 // -*- coding:utf-8 -*-
-// Time-stamp: <Feb 02 2012>
+// Time-stamp: <Feb 08 2012>
 // rename all arguments as a file list
 
 // this program will do
-// - add extension to all files
+// - add extension to files
+// - delete extension to files that already have the extension
 
 // usage:
-// put a shortcut icon of 'renamex' to C:\Users\<YOUR USERNAME>\AppData\Roaming\Microsoft\Windows\SendTo (Windows7) 
+// put a shortcut icon of 'renamex' to
+//   C:\Users\<YOUR USERNAME>\AppData\Roaming\Microsoft\Windows\SendTo (Windows7)
+//   C:\Documents and Settings\<YOUR USERNAME>\SendTo (WindowsXP)
 
 // how to compile (with gnupack-devel 7.02):
-//    gcc -O2 -mwindows -o confexec.exe confexec.c ; strip confexec.exe
+//    gcc -O2 -mwindows -o renamex.exe renamex.c ; strip renamex.exe
 
 #define DEFAULT_EXTENSION ".xx"
 
@@ -32,8 +35,8 @@ int main(int argc, char **argv){
     //
     struct stat sb;
     int i;
-    char extname[256];
-    memset(extname, 0x00, 256);
+    char ext_str[256];
+    memset(ext_str, 0x00, 256);
 
     // command line option
     //
@@ -41,7 +44,7 @@ int main(int argc, char **argv){
         switch(opt){
         case 'e':
             //printf("dir = %s\n", optarg);
-            strncpy(extname, optarg, 255);
+            strncpy(ext_str, optarg, 255);
             break;
         default:
             break;
@@ -55,10 +58,10 @@ int main(int argc, char **argv){
     if(argc < 1) exit(EXIT_FAILURE);
     //printf("error arg (%d) = %s\n", argc, argv[0]); exit(-1);
 
-    // confirm specifying extname
+    // confirm specifying ext_str
     //
-    if (*extname == '\0'){
-        strncpy(extname, DEFAULT_EXTENSION, 255);
+    if (*ext_str == '\0'){
+        strncpy(ext_str, DEFAULT_EXTENSION, 255);
     }
 
     for (i=0; i<argc; i++){
@@ -66,9 +69,20 @@ int main(int argc, char **argv){
             printf("%s is not found\n", argv[i]);
         }else{
             char newname[1024];
+            char *p;
             memset(newname, 0x00, 1024);
             strncpy(newname, argv[i], 1023);
-            strncat(newname, extname, 1023);
+
+            //if newname = "aa.xx", strlen(newname)== 5, strlen(ext_str)==3
+            p = newname + strlen(newname) - strlen(ext_str);
+            if (strncmp(ext_str, p, 1023) == 0){
+                //delete extension
+                 *p = '\0';
+            }else{
+                //add extension
+                strncat(newname, ext_str, 1023);
+            }
+
             if (rename(argv[i], newname)){
                 printf("err: rename %s to %s in fail\n", argv[i], newname);
                 exit(EXIT_FAILURE);
